4decbabbc49476e6d0f9ce890044acdfb183bdde
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-03-18 15:04:04 -0600

    emacs: install company package

--- emacs.nix
+++ emacs.nix
@@ -4,5 +4,5 @@ let emacs = pkgs.emacs;
     emacsWithPackages = (pkgs.emacsPackagesFor emacs).emacsWithPackages;
 in
   emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
-    doom-themes flycheck lsp-mode magit org-superstar
+    company doom-themes flycheck lsp-mode magit org-superstar
   ]) ++ (with epkgs.melpaPackages; [ ccls fira-code-mode ]))
