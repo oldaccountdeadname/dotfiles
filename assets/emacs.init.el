fe109de0cc2f00aec1bd5d52d4858bed139c1aba
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-06-01 16:47:15 -0600

    emacs/init: restructure init file
    hopefully this is actually readable now

--- assets/emacs.init.el
+++ assets/emacs.init.el
@@ -1,50 +1,29 @@
-(defun make-vector (list)
+(require 'calfw-org)
+(require 'fira-code-mode)
+(require 'lsp-haskell)
+(require 'lsp-mode)
+(require 'mu4e)
+(require 'org-superstar)
+(require 'package)
+
+(defun all-the-fancy-things ()
+  (flycheck-mode) (lsp) (company-mode))
+
+(defun make-a-vector (list)
   (vconcat '() list))
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(global-display-line-numbers-mode 1)
-(toggle-scroll-bar -1)
+;; This is some slight witchcraft to determine whether or not some
+;; text is written in Fira. We need to know this to determine whether
+;; or not to apply ligatures.
+(defun is-fira (pos)
+  (let ((font (font-at pos)))
+    (if (not font) nil
+      (let ((family (font-get font :family)))
+	(if (not family) nil
+	  (or (string= "Fira Code" family)
+	      (string= "FiraCode"  family)))))))
 
-(setq x-select-enable-clipboard t)
-
-;; i just copied this off of LSP docs - they seem aggressive, but they certainly
-;; work.
-(setq gc-cons-threshold 100000000)
-(setq read-process-output-max (* 1024 1024))
-
-(setq inhibit-startup-message t)
-
-(setq-default header-line-format mode-line-format)
-(setq-default mode-line-format '())
-
-(add-hook 'conf-unix-mode-hook '(lambda ()
-				  (local-set-key (kbd "RET")
-						 'electric-newline-and-maybe-indent)))
-
-(add-hook 'text-mode-hook '(lambda ()
-			     (flyspell-mode)
-			     (local-set-key (kbd "RET")
-					    'electric-newline-and-maybe-indent)))
-
-(add-hook 'prog-mode-hook '(lambda ()
-			     (flyspell-prog-mode)
-			     (local-set-key (kbd "RET")
-					    'electric-newline-and-maybe-indent)
-			     (set 'display-fill-column-indicator-column 80)
-			     (display-fill-column-indicator-mode)))
-
-(add-hook 'haskell-mode-hook #'lsp)
-
-(setq-default visual-fill-column-width 80)
-(add-hook 'visual-line-mode-hook '(lambda ()
-				    (setq visual-fill-column-center-text t)
-				    (visual-fill-column-mode)))
-
-(require 'package)
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;;; Keybindings
 
 (defconst up    (kbd "C-n"))
 (defconst down  (kbd "C-s"))
@@ -118,117 +97,66 @@
 (global-set-key (kbd "C-x f") 'counsel-find-file)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fira-code-mode-disabled-ligatures '("x"))
- '(org-agenda-files '("/mnt/remote/org/default.org"))
- '(package-selected-packages
-   '(acme-theme org-superstar magit flycheck lsp-mode dracula-theme fira-code-mode f ##)))
+;;; Basic UI
 
-; The below apply ligatures *only to text in Fira Code*, which
-;; prevents weirdness in variable-pitch-mode buffers. It's a bit of a
-;; broad solution, but end behavior is perfectly fine.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(toggle-scroll-bar -1)
+(setq inhibit-startup-message t)
 
-(defun is-fira (pos)
-  (let ((font (font-at pos)))
-    (if (not font) nil
-      (let ((family (font-get font :family)))
-	(if (not family) nil
-	  (or (string= "Fira Code" family)
-	      (string= "FiraCode"  family)))))))
+(setq-default header-line-format mode-line-format)
+(setq-default mode-line-format '())
 
-(setq-default prettify-symbols-compose-predicate
-	     (lambda (start end match)
-		(and (prettify-symbols-default-compose-p start end match)
-		    (is-fira start))))
+(setq-default visual-fill-column-width 80)
 
-(require 'fira-code-mode)
-(fira-code-mode-set-font)
-(global-fira-code-mode)
-(set-frame-font "Fira Code 10" nil t)
+;;; Basic Functionality
 
-(set-face-font 'default ":antialias=true")
-(load-theme 'acme 't)
+(setq x-select-enable-clipboard t)
 
-(setq company-minimum-prefix-length 1
-      company-idle-delay 0.1)
+(add-hook 'conf-unix-mode-hook
+	  '(lambda ()
+	     (flyspell-mode)
+	     (local-set-key (kbd "RET")
+			    'electric-newline-and-maybe-indent)))
 
-(setq-default lsp-enable-on-type-formatting nil)
+(add-hook 'text-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "RET")
+			    'electric-newline-and-maybe-indent)))
+
+(add-hook 'prog-mode-hook '(lambda ()
+			     (flyspell-prog-mode)
+			     (local-set-key (kbd "RET")
+					    'electric-newline-and-maybe-indent)
+			     (set 'display-fill-column-indicator-column 80)
+			     (display-fill-column-indicator-mode)))
+
+(add-hook 'c-mode-common-hook #'all-the-fancy-things)
+(add-hook 'python-mode-hook   #'all-the-fancy-things)
+(add-hook 'haskell-mode-hook  #'all-the-fancy-things)
+
+(add-to-list 'auto-mode-alist '("\\.nix\\'" . prog-mode))
+
 (setq-default c-default-style "bsd"
 	      c-basic-offset 8
 	      indent-tabs-mode t)
 
-(add-to-list 'auto-mode-alist '("\\.nix\\'" . prog-mode))
+;;; Company
 
-(require 'lsp-mode)
-(require 'lsp-haskell)
+(setq company-minimum-prefix-length 1
+      company-idle-delay 0.1)
+
+;;; LSP Stuff
+
+;; We allow environment variables to control the language server for
+;; more flexibility in the NixOS configuration.
 (setq lsp-clangd-binary-path (getenv "LANGSERV_CC"))
 (setq lsp-haskell-server-path (getenv "LANGSERV_HK"))
 
-(add-hook 'c-mode-common-hook '(lambda ()
-			  (flycheck-mode)
-			  (lsp)
-			  (company-mode)))
+(setq-default lsp-enable-on-type-formatting nil)
 
-(add-hook 'python-mode-hook '(lambda ()
-			       (flycheck-mode)
-			       (lsp)
-			       (company-mode)))
-
-(require 'calfw-org)
-(setq calendar-holidays nil)
-
-(setq calendar-day-name-array (make-vector
-	       (mapcar #'downcase calendar-day-name-array)))
-
-(setq calendar-month-name-array (make-vector
-	       (mapcar #'downcase calendar-month-name-array)))
-
-(require 'org-superstar)
-(setq org-hide-leading-stars t)
-(setq org-superstar-remove-leading-stars t)
-(setq org-superstar-leading-bullet ?\s)
-(setq org-superstar-headline-bullets-list '(" ")
-      org-superstar-item-bullet-alist
-      '((?* . ?•)
-	(?+ . ?›)
-	(?- . ?–)))
-
-(add-hook 'org-mode-hook '(lambda ()
-			    (setq line-spacing 4)
-			    (setq cursor-type 'bar)
-			    (setq org-hide-emphasis-markers t)
-			    (visual-line-mode)
-			    (org-indent-mode 1)
-			    (org-superstar-mode 1)
-			    (display-line-numbers-mode -1)
-			    (variable-pitch-mode 1)))
-
-(setq mu4e-compose-signature
-"Lincoln Auster
-they/them")
-
-(setq user-full-name "lincoln auster [they/them]"
-      user-mail-address "lincolnauster@gmail.com")
-
-(setq send-mail-function 'smtpmail-send-it
-      smtpmail-smtp-service 587
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-servers-requiring-authorization ".*"
-      smtpmail-stream-type 'starttls
-      smtpmail-auth-credentials
-        '(("smtp.gmail.com" 587 "lincolnauster@gmail.com"
-           (string-trim
-	    (shell-command-to-string
-	     "gpg -q --batch -q --decrypt ~/.config/gmail-pass.gpg")))))
-
-(add-to-list 'load-path "/run/current-system/sw/share/emacs/site-lisp/mu4e")
-(require 'mu4e)
-
-
+;;; Custom(izations) and Fonts
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -258,3 +186,83 @@ they/them")
  '(cfw:face-toolbar ((t (:inherit cfw:face-day-title)))))
 
 (set-frame-font "Fira Code 10" nil t)
+
+(custom-set-variables
+ '(fira-code-mode-disabled-ligatures '("x"))
+ '(org-agenda-files '("/mnt/remote/org/default.org"))
+ '(package-selected-packages
+   '(acme-theme org-superstar magit flycheck lsp-mode dracula-theme fira-code-mode f ##)))
+
+(setq-default prettify-symbols-compose-predicate
+	     (lambda (start end match)
+		(and (prettify-symbols-default-compose-p start end match)
+		    (is-fira start))))
+
+(fira-code-mode-set-font)
+(global-fira-code-mode)
+(set-frame-font "Fira Code 10" nil t)
+
+(set-face-font 'default ":antialias=true")
+
+;;; Org mode stuff
+(setq calendar-holidays nil)
+
+(setq calendar-day-name-array (make-a-vector
+	       (mapcar #'downcase calendar-day-name-array)))
+
+(setq calendar-month-name-array (make-a-vector
+	       (mapcar #'downcase calendar-month-name-array)))
+
+(setq org-hide-leading-stars t)
+(setq org-superstar-remove-leading-stars t)
+(setq org-superstar-leading-bullet ?\s)
+(setq org-superstar-headline-bullets-list '(" ")
+      org-superstar-item-bullet-alist
+      '((?* . ?•)
+	(?+ . ?›)
+	(?- . ?–)))
+
+(add-hook 'org-mode-hook '(lambda ()
+			    (setq line-spacing 4)
+			    (setq cursor-type 'bar)
+			    (setq org-hide-emphasis-markers t)
+			    (visual-line-mode)
+			    (org-indent-mode 1)
+			    (org-superstar-mode 1)
+			    (display-line-numbers-mode -1)
+			    (variable-pitch-mode 1)))
+
+;;; mu4e stuff
+(setq mu4e-compose-signature
+"Lincoln Auster
+they/them")
+
+(setq user-full-name "lincoln auster [they/them]"
+      user-mail-address "lincolnauster@gmail.com")
+
+(setq send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-service 587
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-servers-requiring-authorization ".*"
+      smtpmail-stream-type 'starttls
+      smtpmail-auth-credentials
+        '(("smtp.gmail.com" 587 "lincolnauster@gmail.com"
+           (string-trim
+	    (shell-command-to-string
+	     "gpg -q --batch -q --decrypt ~/.config/gmail-pass.gpg")))))
+
+(add-to-list 'load-path "/run/current-system/sw/share/emacs/site-lisp/mu4e")
+
+;;; Aesthetic Adjustments
+
+(load-theme 'acme 't)
+(add-hook 'visual-line-mode-hook '(lambda ()
+				    (setq visual-fill-column-center-text t)
+				    (visual-fill-column-mode)))
+
+;;; Nuts + bolts
+
+;; aggressive settings copied off of LSP docs
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
